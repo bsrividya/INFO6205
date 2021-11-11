d4772d5a7d6407b94f61985a69bd6b171b0f6000
bsrividya <burra.sr@northeastern.edu>
2021-11-11 15:23:25 -0500

    parallel sort assignment

--- src/main/java/edu/neu/coe/info6205/sort/par/Main.java
+++ src/main/java/edu/neu/coe/info6205/sort/par/Main.java
@@ -18,18 +18,20 @@ public class Main {
 
     public static void main(String[] args) {
         processArgs(args);
-        System.out.println("Degree of parallelism: " + ForkJoinPool.getCommonPoolParallelism());
+        int thread = 64;
+        ForkJoinPool threadpool = new ForkJoinPool(thread);
+        System.out.println("Degree of parallelism: " + threadpool.getParallelism());
         Random random = new Random();
         int[] array = new int[2000000];
         ArrayList<Long> timeList = new ArrayList<>();
-        for (int j = 50; j < 100; j++) {
+        for (int j = 0; j < 20;j++) {
             ParSort.cutoff = 10000 * (j + 1);
             // for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
             long time;
             long startTime = System.currentTimeMillis();
             for (int t = 0; t < 10; t++) {
                 for (int i = 0; i < array.length; i++) array[i] = random.nextInt(10000000);
-                ParSort.sort(array, 0, array.length);
+                ParSort.sort(array, 0, array.length, threadpool);
             }
             long endTime = System.currentTimeMillis();
             time = (endTime - startTime);
@@ -45,7 +47,7 @@ public class Main {
             BufferedWriter bw = new BufferedWriter(isr);
             int j = 0;
             for (long i : timeList) {
-                String content = (double) 10000 * (j + 1) / 2000000 + "," + (double) i / 10 + "\n";
+                String content = (double) 10000 * (j + 1) + "," + (double) i + "\n";
                 j++;
                 bw.write(content);
                 bw.flush();
